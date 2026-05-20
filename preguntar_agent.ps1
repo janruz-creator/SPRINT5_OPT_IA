8130ce994e940b166be46caff92957a637dc53aa
Julen-Paez <julenpaez@iesmontsia.org>
2026-05-20 18:21:50 +0200

    Limpiar archivos de la raíz

--- preguntar_agent.ps1
+++
@@ -1,3 +0,0 @@
-$pregunta = Read-Host "Escribe tu pregunta"
-Invoke-RestMethod -Uri "http://localhost:5678/webhook-test/agentb" -Method POST -ContentType "application/json" -Body "{`"body`":{`"content`":`"$pregunta`"}}"
-Write-Host "Pregunta enviada!"
